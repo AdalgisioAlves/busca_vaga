7e79ed1e67ed9fc549d5be2f519b4d17f00c1d8a
Adalgisio Alves Figueiredo <adalgisio.figueiredo@acpgroup.com.br>
2021-03-27 03:31:09 -0300

    Ajuste na paginação

--- src/HtmlToArray.py
+++ src/HtmlToArray.py
@@ -5,30 +5,32 @@ import time as time
 from src import db
 import sys
 import os
-os.environ['MOZ_HEADLESS'] = '1'
+#os.environ['MOZ_HEADLESS'] = '1'
 
 
 class htmlToArray():
     
-    def scrapLinkedin(br):  
-                
+    def scrapLinkedin(br):
+        
+        
         br.get('https://www.linkedin.com/jobs/search/?currentJobId=2447206536&geoId=106057199&keywords=php&location=Brasil')
-        br.implicitly_wait(30)
-        time.sleep(7)
-        paginacao = br.find_elements_by_class_name("artdeco-pagination__pages")[0].find_elements(By.TAG_NAME,"button") 
-        #paginacao.remove(0)
-        pa = 0
-        for btn in paginacao :
-            if pa > 0:
-                btn.click()
-                time.sleep(5)
+        time.sleep(8)
+        n_paginas = len(br.find_elements_by_class_name("artdeco-pagination__pages")[0].find_elements(By.TAG_NAME,"button") )
+        
+        p = 0
+        while n_paginas > p:
+            if p > 0:
+                br.refresh();
+                time.sleep(3)
+                btn = br.find_elements_by_class_name("artdeco-pagination__pages")[0].find_elements(By.TAG_NAME,"button")
+                btn[p].click()
+                time.sleep(10)
                 
             jobs_ul = br.find_elements_by_class_name("jobs-search-results__list-item") 
-            
-            pa = pa + 1   
+                
             rs = db.DB()
             for li in jobs_ul:                
-                try:                    
+                                
                     a       = li.find_elements(By.TAG_NAME,"a")   
                     if len(a) >2:
                         
@@ -58,13 +60,10 @@ class htmlToArray():
                             print('Registro já existe no banco')
                         else:
                             print('Registro inserido ',rs)
-                except :
-                    print("Erro desconhecido:", sys.exc_info())
+            p = p + 1
                 
-             
-        
-        br.close()
-        return True
+            
+            
        
      
         
